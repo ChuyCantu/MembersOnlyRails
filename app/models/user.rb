8b4dc30aa8b265634412466e1c62796a04cce789
Jesus Cantu <chuy_cr11@hotmail.com>
2022-06-24 18:26:31 -0500

    Fixed user account deletion.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many(:posts)
+  has_many(:posts, dependent: :destroy)
+
+  validates(:name, presence: true)
+  validates(:email, presence: true)
+  validates(:password, presence: true, length: { minimum: 6 })
 end
